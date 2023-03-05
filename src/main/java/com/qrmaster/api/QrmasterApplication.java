5b1b3dee8f8989a7b9b35918c2a39affc38c6593
이형준 <swk01139@gmail.com>
2023-03-05 21:38:59 +0900

    initial commit

---
+++ src/main/java/com/qrmaster/api/QrmasterApplication.java
@@ -0,0 +1,13 @@
+package com.qrmaster.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QrmasterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QrmasterApplication.class, args);
+    }
+
+}
