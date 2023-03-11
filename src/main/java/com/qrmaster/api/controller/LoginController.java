3de9c57513aa118fdd7dadcec289c0b5ef3a90eb
이형준 <swk01139@gmail.com>
2023-03-11 15:09:21 +0900

    java 11 설정 변경

--- src/main/java/com/qrmaster/api/controller/LoginController.java
+++ src/main/java/com/qrmaster/api/controller/LoginController.java
@@ -8,7 +8,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@Slf4j
+//@Slf4j
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/login")
