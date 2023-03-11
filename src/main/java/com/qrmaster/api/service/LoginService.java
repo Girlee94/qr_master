35d18adb374972b07e980f533830b3d47bf654a8
이형준 <swk01139@gmail.com>
2023-03-11 16:52:13 +0900

    log4j2 설정 적용

--- src/main/java/com/qrmaster/api/service/LoginService.java
+++ src/main/java/com/qrmaster/api/service/LoginService.java
@@ -7,7 +7,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
-//@Slf4j
+@Slf4j
 @RequiredArgsConstructor
 @Service
 public class LoginService {
